82069449f82837ee9d4ff425dc72976df7e5fde8
abedfetrat <abedfetrat@gmail.com>
2024-08-09 23:28:10 +0200

    feat: fetch posts and display them

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -12,8 +12,23 @@ interface UsersListResponse {
   users: User[]
 }
 
+
+interface Post {
+  id: string,
+  author: string,
+  content: string,
+  likes: number,
+  dislikes: number,
+  postedAt: Date,
+}
+
+interface PostsListResponse {
+  posts: Post[]
+}
+
 function App() {
   const [user, setUser] = useState<User | null>(null);
+  const [posts, setPosts] = useState<Post[] | null>(null);
 
   useEffect(() => {
     fetch("data/users.json")
@@ -21,6 +36,14 @@ function App() {
       .then(usersListResponse => usersListResponse as UsersListResponse)
       .then(usersListResponse => setUser(usersListResponse.users[0]))
       .catch(_ => console.error("Could not fetch users."));
+  }, []);
+
+  useEffect(() => {
+    fetch("data/posts.json")
+      .then(response => response.json())
+      .then(postsListResponse => postsListResponse as PostsListResponse)
+      .then(postsListResponse => setPosts(postsListResponse.posts))
+      .catch(_ => console.error("Could not fetch posts."));
   }, []);
 
   return (
@@ -37,6 +60,19 @@ function App() {
               <p>Email: {user.email}</p>
             </div>
           </div>
+          :
+          "Loading user..."
+      }
+      {
+        posts != null
+          ?
+          posts.map(post => (
+            <div className="card bg-base-100 w-96 shadow-xl">
+              <div className="card-body">
+                <p>{post.content}</p>
+              </div>
+            </div>
+          ))
           :
           "Loading user..."
       }
